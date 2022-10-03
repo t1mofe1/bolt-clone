3e0387033e799ab839aed904c210880d8335ed4c
T1M0FE1 <timofei.magi@gmail.com>
2022-10-04 00:52:49 +0300

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { ChangeEventHandler, useState } from 'react';
+import { BoltMap } from './components/BoltMap';
 import { BottomBar } from './components/BottomBar';
 import { SideBar } from './components/SideBar';
-import { BoltMap } from './components/BoltMap';
 import { MainContainer } from './styles';
 
 function App() {
